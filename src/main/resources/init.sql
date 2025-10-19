710d11f959e024c3b4cee2fda06f4ab2ba6e7153
Raphides <raphaelmendesdasilva@gmail.com>
2025-10-19 19:57:25 -0300

    role e user sairam de 'muitos para um' para 'muitos para muitos'

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,8 +5,8 @@ CREATE TABLE scenario (
     period TSRANGE NOT NULL
 );
 
-CREATE TABLE bussiness_hour(
-    id_bussiness_hour BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE business_hour(
+    id_business_hour BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     activated BOOLEAN NOT NULL DEFAULT TRUE,
     start_week_day SMALLINT NOT NULL CHECK (start_week_day > 0 AND start_week_day < 8),
     end_week_day SMALLINT NOT NULL CHECK (end_week_day > 0 AND end_week_day < 8),
@@ -19,17 +19,17 @@ CREATE TABLE price_model(
     id_price_model BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     model_name VARCHAR(50) NOT NULL,
     activated BOOLEAN NOT NULL DEFAULT TRUE,
-    price NUMERIC NOT NULL CHECK (price > 0),
+    price NUMERIC(38,2) NOT NULL CHECK (price > 0),
     id_scenario BIGINT NOT NULL REFERENCES scenario (id_scenario)
 );
 
 CREATE TABLE operation(
     id_operation BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    entry TIMESTAMP NOT NULL,
-    leave TIMESTAMP,
+    entry TIMESTAMP WITH TIME ZONE NOT NULL,
+    leave TIMESTAMP WITH TIME ZONE,
     plate VARCHAR(7) NOT NULL CHECK (plate ~ '[A-Z]{3}[0-9]{4}' OR plate ~ '[A-Z]{3}[0-9][A-Z][0-9]{2}'),
-    total NUMERIC CHECK (total > -1),
-    id_price_model INTEGER NOT NULL REFERENCES price_model (id_price_model)
+    total NUMERIC(38,2) CHECK (total > -1),
+    id_price_model BIGINT NOT NULL REFERENCES price_model (id_price_model)
 );
 
 CREATE TABLE estaciona_role(
@@ -40,7 +40,11 @@ CREATE TABLE estaciona_role(
 CREATE TABLE estaciona_user(
     id_user INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50) NOT NULL,
-    password_hash TEXT NOT NULL,
-    id_role INT NOT NULL REFERENCES estaciona_role (id_role)
+    password_hash TEXT NOT NULL
 );
 
+CREATE TABLE estaciona_user_role(
+    id_user_role INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id_user INT NOT NULL REFERENCES estaciona_user (id_user),
+    id_role INT NOT NULL REFERENCES estaciona_role (id_role)
+);
