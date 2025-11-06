00090b79a40d1c0d2b327efe68656a3e3f3e5cf8
Raphides <raphaelmendesdasilva@gmail.com>
2025-11-06 10:13:16 -0300

    União do DDL e DML iniciais em um único script

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -36,3 +36,7 @@ CREATE TABLE operation(
     paid BOOLEAN NOT NULL DEFAULT FALSE,
     id_price_model BIGINT NOT NULL REFERENCES price_model (id_price_model)
 );
+
+INSERT INTO scenario(scenario_name, capacity, scenario_start, scenario_end) VALUES ('dia-a-dia', 30, '1970-01-01 00:00:00-03', '9999-12-31 23:59:59-03');
+INSERT INTO price_model(model_name, price, frequency_type, id_scenario) VALUES ('horista', 4, 'HOUR', 1);
+INSERT INTO business_hour (start_week_day, end_week_day, start_time, end_time, id_scenario) VALUES (1, 5, TIME '10:00' , TIME '18:00', 1);
